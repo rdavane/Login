41f3bed0caa5fae7274537eb3641a996af7faebb
rdavane <rahul.davane006@gmail.com>
2017-12-15 00:10:56 +0530

    insert new entry
    add new entry using hibernate query

--- src/main/java/com/rahul/controller/UserDetailsController.java
+++ src/main/java/com/rahul/controller/UserDetailsController.java
@@ -5,11 +5,16 @@
  */
 package com.rahul.controller;
 
+import com.rahul.model.UserDetails;
+import com.rahul.service.AllInsertService;
 import com.rahul.service.AllViewService;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -21,11 +26,26 @@ public class UserDetailsController {
 
     @Autowired
     AllViewService viewService;
+    @Autowired
+    AllInsertService insertService;
 
     @RequestMapping(value = "success")
     public ModelAndView success(HttpSession session) {
         ModelAndView modelAndView = new ModelAndView("Success");
         modelAndView.addObject("userdt", viewService.getanyhqldatalist("from userdetails"));
         return modelAndView;
+    }
+
+    @RequestMapping(value = "insertuser")
+    public String insertuser(@RequestParam(value = "username") String username,
+            @RequestParam(value = "password") String password) {
+        UserDetails details = new UserDetails();
+        details.setPassword(password);
+        details.setUsername(username);
+        List<Map<String, Integer>> map = viewService.getanyjdbcdatalist("select max(id) id from userdetails");
+        int id = map.get(0).get("id") + 1;
+        details.setId(id);
+        insertService.insert(details);
+        return "redirect:success";
     }
 }
